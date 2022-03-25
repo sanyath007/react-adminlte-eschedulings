09fef46701261b60860621738c0e45d17f071a64
sanyath007 <sanyath007@gmail.com>
2022-03-25 15:49:41 +0700

    Modify jQuery script of MainSidebar layout

--- src/layouts/MainSidebar/index.js
+++ src/layouts/MainSidebar/index.js
@@ -5,27 +5,31 @@ import { Link } from 'react-router-dom';
 const MainSidebar = () => {
 
   useEffect(() => {
-    // window.$(function () {
-    //   /** menu */
-    //   window.$(".nav-sidebar > .nav-item > .nav-link").on("click", function (e) {
-    //     /** Remove .menu-open class from previous opened .nav-item */
-    //     window.$(".nav-sidebar").find(".menu-open").removeClass("menu-is-opening menu-open");
-
-    //     /** Set class and style to clicked .nav-item */
-    //     window.$(this).parent().addClass("menu-is-opening menu-open");
-
-    //     /** Remove .active class from previous actived .nav-link that is child of .nav-item */
-    //     window.$(".nav-sidebar .nav-item").find(".active").removeClass("active");
-    //     /** Set .active class to clicked .nav-link */
-    //     window.$(this).addClass("active");
-    //   });
-
-    //   /** submenu */
-    //   window.$(".nav-treeview .nav-link").on("click", function (e) {
-    //     window.$(".nav-treeview").find(".active").removeClass("active");
-    //     window.$(this).addClass("active");
-    //   });
-    // });
+    window.$(function() {
+      /** menu */
+      window.$(".nav-sidebar > .nav-item > .nav-link").on("click", function (e) {
+        console.log('sidebar click...');
+    
+        /** Remove .menu-open class from previous opened .nav-item */
+        window.$(".nav-sidebar").find(".menu-open").removeClass("menu-is-opening menu-open");
+    
+        /** Set class and style to clicked .nav-item */
+        if(window.$(this).parent().hasClass("has-treeview")) {
+          window.$(this).parent().addClass("menu-is-opening menu-open");
+        }
+    
+        /** Remove .active class from previous actived .nav-link that is child of .nav-item */
+        window.$(".nav-sidebar .nav-item").find(".active").removeClass("active");
+        /** Set .active class to clicked .nav-link */
+        window.$(this).addClass("active");
+      });
+    
+      /** submenu */
+      window.$(".nav-treeview .nav-link").on("click", function (e) {
+        window.$(".nav-treeview").find(".active").removeClass("active");
+        window.$(this).addClass("active");
+      });
+    });
   }, []);
 
   return (
