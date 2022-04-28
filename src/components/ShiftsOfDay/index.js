fb8504ed66107ef6cd6651ac83f217c73d158918
sanyath007 <sanyath007@gmail.com>
2022-04-28 10:48:46 +0700

    Modify ShiftsOfDay by convert otShift prop to array and set to otShift props of ShiftText

--- src/components/ShiftsOfDay/index.js
+++ src/components/ShiftsOfDay/index.js
@@ -4,14 +4,19 @@ import ShiftText from '../ShiftText';
 const ShiftsOfDay = ({ shifts, otShift, onSetOT }) => {
   return (
     <>
-      {shifts.split('|').length > 0 && shifts.split('|').map((el, index) => (
-        <ShiftText
-          key={index+el}
-          text={el}
-          otShift={otShift}
-          onSetOT={onSetOT}
-        />
-      ))}
+      {shifts.split('|').length > 0 && shifts.split('|').map((el, index) => {
+        /** แปลง otShift เป็น array โดยถ้ามีเวร OT มากกว่า 1 จะถูกแยกด้วยเครื่องหมาย | */
+        const arrOtShift = otShift.split('|');
+
+        return (
+          <ShiftText
+            key={index+el}
+            text={el}
+            otShift={arrOtShift.length > 1 ? arrOtShift[index] : arrOtShift[0]}
+            onSetOT={onSetOT}
+          />
+        );
+      })}
     </>
   );
 }
