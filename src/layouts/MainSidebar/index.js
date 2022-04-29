a3afae483bfa48ae20513feb17105e702449427b
sanyath007 <sanyath007@gmail.com>
2022-04-29 07:12:24 +0700

    Tryout to move code for manipulating treeview menu and active menu from MainSidebar to App

--- src/layouts/MainSidebar/index.js
+++ src/layouts/MainSidebar/index.js
@@ -1,37 +1,7 @@
 import React from 'react';
-import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 const MainSidebar = () => {
-
-  useEffect(() => {
-    window.$(function() {
-      /** menu */
-      window.$(".nav-sidebar > .nav-item > .nav-link").on("click", function (e) {
-        console.log('sidebar click...');
-    
-        /** Remove .menu-open class from previous opened .nav-item */
-        window.$(".nav-sidebar").find(".menu-open").removeClass("menu-is-opening menu-open");
-    
-        /** Set class and style to clicked .nav-item */
-        if(window.$(this).parent().hasClass("has-treeview")) {
-          window.$(this).parent().addClass("menu-is-opening menu-open");
-        }
-    
-        /** Remove .active class from previous actived .nav-link that is child of .nav-item */
-        window.$(".nav-sidebar .nav-item").find(".active").removeClass("active");
-        /** Set .active class to clicked .nav-link */
-        window.$(this).addClass("active");
-      });
-    
-      /** submenu */
-      window.$(".nav-treeview .nav-link").on("click", function (e) {
-        window.$(".nav-treeview").find(".active").removeClass("active");
-        window.$(this).addClass("active");
-      });
-    });
-  }, []);
-
   return (
     <aside className="main-sidebar sidebar-dark-primary elevation-4">
       {/* Brand Logo */}
