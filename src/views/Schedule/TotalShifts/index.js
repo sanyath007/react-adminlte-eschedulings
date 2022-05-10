79edb2b0d16fd7e28596995f3a5e2af85509e98b
sanyath007 <sanyath007@gmail.com>
2022-05-10 15:13:55 +0700

    Reset TotalShifts to old version

--- src/views/Schedule/TotalShifts/index.js
+++ src/views/Schedule/TotalShifts/index.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { calculateShiftsTotal } from '../../../utils';
 
 const initialTotal = {
     night: 0,
@@ -11,10 +10,28 @@ const initialTotal = {
 const TotalShifts = ({ shifts, shiftOfDay }) => {
     const [total, setTotal] = useState(initialTotal);
 
-    useEffect(() => {
-        const shiftsTotal = calculateShiftsTotal(shifts);
+    const calculateTotal = () => {
+        let tmpTotal = { ...initialTotal };
 
-        setTotal(shiftsTotal);
+        shifts.forEach((shift, day) => {
+            for (let i = 1; i <= 3; i++) {
+                if (['ด','ด*','ด**','ด^'].includes(shift[`${day}_${i}`])) {
+                    tmpTotal.night += 1;
+                } else if (['ช','ช*','ช**','ช^'].includes(shift[`${day}_${i}`])) {
+                    tmpTotal.morn += 1;
+                } else if (['บ','บ*','บ**','บ^'].includes(shift[`${day}_${i}`])) {
+                    tmpTotal.even += 1;
+                } else if (['B','B*','B**','B^'].includes(shift[`${day}_${i}`])) {
+                    tmpTotal.bd += 0.5;
+                }
+            }
+        });
+
+        setTotal(tmpTotal);
+    };
+
+    useEffect(() => {
+        calculateTotal();
     }, [shifts, shiftOfDay]);
 
     return (
