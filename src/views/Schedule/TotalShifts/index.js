dd23b2e77c136c4dd469b6697c0548d77af641a6
sanyath007 <sanyath007@gmail.com>
2022-05-10 14:51:47 +0700

    Modify TotalShifts by removing calculateTotal method and using calculateShiftsTotal of helper function of utils instead.

--- src/views/Schedule/TotalShifts/index.js
+++ src/views/Schedule/TotalShifts/index.js
@@ -1,4 +1,5 @@
-import React, { useEffect, useState, useRef } from 'react'
+import React, { useEffect, useState } from 'react';
+import { calculateShiftsTotal } from '../../../utils';
 
 const initialTotal = {
     night: 0,
@@ -10,28 +11,10 @@ const initialTotal = {
 const TotalShifts = ({ shifts, shiftOfDay }) => {
     const [total, setTotal] = useState(initialTotal);
 
-    const calculateTotal = () => {
-        let tmpTotal = { ...initialTotal };
-
-        shifts.forEach((shift, day) => {
-            for (let i = 1; i <= 3; i++) {
-                if (['ด','ด*','ด**','ด^'].includes(shift[`${day}_${i}`])) {
-                    tmpTotal.night += 1;
-                } else if (['ช','ช*','ช**','ช^'].includes(shift[`${day}_${i}`])) {
-                    tmpTotal.morn += 1;
-                } else if (['บ','บ*','บ**','บ^'].includes(shift[`${day}_${i}`])) {
-                    tmpTotal.even += 1;
-                } else if (['B','B*','B**','B^'].includes(shift[`${day}_${i}`])) {
-                    tmpTotal.bd += 0.5;
-                }
-            }
-        });
-
-        setTotal(tmpTotal);
-    };
-
     useEffect(() => {
-        calculateTotal();
+        const shiftsTotal = calculateShiftsTotal(shifts);
+
+        setTotal(shiftsTotal);
     }, [shifts, shiftOfDay]);
 
     return (
@@ -41,7 +24,7 @@ const TotalShifts = ({ shifts, shiftOfDay }) => {
             <span>บ={total.even}</span>
             <span>B={total.bd}</span>
         </div>
-    )
-}
+    );
+};
 
-export default TotalShifts
+export default TotalShifts;
